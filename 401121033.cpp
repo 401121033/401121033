9af7269e7a4988f029f045749cdac4a9f1e8bb2d
401121033 <146569812+401121033@users.noreply.github.com>
2023-11-07 23:36:45 +0330

    401121033
    mobina fallahazad

--- 401121033.cpp
+++ 401121033.cpp
@@ -1,12 +1,34 @@
 #include<iostream>
+#include<conio.h>
 using namespace std ;
-int main()
+
+class MATH {
+	 public:
+	 	string isPrime(int x )
+	 	{
+	 		int a = 0 , i=2 ;
+	 		while (i<x )
+	 		{
+	 			if (x%i==0)
+	 			a++;
+	 			i++;
+			 }
+	 		if(a==0)
+	 		return "true";
+	 		else 
+	 		return "false ";
+		 }
+		
+};
+
+int main ()
 {
-	char c[]="Hello c++.";
+	MATH y ;
+	int x ;
+	 cout << " enter x : ";
+	 cin >> x ;
+	 cout << y.isPrime(x);
+	 return 0 ;
 	
-	cout<<'\n'<<"Hello World.";
-	
-	cout<<'\n'<<c;
-	
-	return 0;
+	getch();
 }
